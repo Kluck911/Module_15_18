253c15b5f0d10066662c5876bf5d20f67c40b7ee
kluck911 <kluck1812@gmail.com>
2021-08-04 15:16:36 +0300

    15_4 добавить фильтр слов

--- 15_4_practice/1_longer_word.py
+++ 15_4_practice/1_longer_word.py
@@ -7,16 +7,17 @@ def clean_text(text):
     for i in range(len(text)):
         if (list_text[i].isalpha() == False) and list_text[i] != ' ':
             text = text.replace(list_text[i], ' ')
-    return text
+    return text.lower()
+
+
+def filer_less_3_letter(word_)
 
 
 
 
 
 with open(get_filename(), encoding='utf8') as f:
-    line = f.read()
-    clean_text(line)
-    line = clean_text(line)
+    line = clean_text(f.read()).split()
     print(line)
 
 ''''
