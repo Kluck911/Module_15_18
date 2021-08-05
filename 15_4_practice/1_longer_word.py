3f002cc14e8514807f8fa81daced11c895601695
kluck911 <kluck1812@gmail.com>
2021-08-05 13:49:35 +0300

    + first done

--- 15_4_practice/1_longer_word.py
+++ 15_4_practice/1_longer_word.py
@@ -1,35 +1,50 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+
 def get_filename():
     fileName = input(f"Введите имя открываемого файла: ")
 
     return fileName + '.txt'
 
 
-def filter_clean(text):
-    list_text = text
+def filter_clean(list_):
+    list_text = list_
 
-    for i in range(len(text)):
+    for i in range(len(list_)):
         if (not list_text[i].isalpha()) and list_text[i] != ' ':
-            text = text.replace(list_text[i], ' ')
+            list_ = list_.replace(list_text[i], ' ')
 
-    return text.lower()
+    return list_.lower()
 
 
 def longest_word(list_):
-    first_word = list_[0]
-    count = 0
+    firstWord = ''
+    listEN = []
+    list_en_filter = []
 
-    for char in list_:
-        if len(char) > len(first_word):
-            first_word = char
+    for word_ in list_:
+        if word_is_en(word_):
+            listEN.append(word_)
+    listEN = sorted(listEN, key=len)[::-1]
+    i = 0
+    list_en_filter.append(listEN[i])
+    while len(listEN[i]) == len(listEN[i+1]):
+        if listEN[i+1] not in list_en_filter:
+            list_en_filter.append(listEN[i + 1])
+        i += 1
 
-    return
+    return ', '.join(list_en_filter)
 
 
-def list_is_EN(list_):
 
-    return
+
+def word_is_en(word_):
+    for i in range(0, len(word_)):
+        for char in word_[i]:
+            if char not in alphabet:
+                return False
+
+    return True
 
 
 def often_word(list_):
@@ -40,8 +55,8 @@ def often_word(list_):
     for i in range(len(list_)):
         countTemp = 0
         first_world = list_[i]
-        for char in range(i, len(list_)-1):
-            if first_world == list_[i+1]:
+        for word_ in range(i, len(list_) - 1):
+            if first_world == list_[i + 1]:
                 countTemp += 1
         if countTemp > countFirst:
             wordResult = first_world
@@ -55,31 +70,15 @@ def often_word(list_):
 def less_3_letter(list_):
     sortList = []
 
-    for char in list_:
-        if len(char) > 3:
-            sortList.append(char)
+    for word_ in list_:
+        if len(word_) > 3:
+            sortList.append(word_)
 
     return sortList
-
 
 
 with open(get_filename(), encoding='utf8') as f:
     wordList = filter_clean(f.read()).split()
     often_word(wordList)
-    print(wordList)
-    print(f'Наиболее часто встречающееся слово: {often_word(wordList)}')
-
-''''
- 
-    line = line.replace('!', '')
-    line = line.replace(',', '')
-    line = line.replace('.', '')
-    print(line)
-    line.split()
-    print(line.split())
-    for n in len(fileList):
-       print(fileList[n])
-         text = text.replace(" ", "")
-    text = text.replace("\n", "")
-
-'''
+    print(f'Наиболее часто встречающееся слово:\n{often_word(wordList)}')
+    print(f'Наиболее длинное слово на английском:\n {longest_word(wordList)}')
