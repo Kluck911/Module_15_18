f283eeabaf420c2a6d5ff674db51df8c72182db1
kluck911 <kluck1812@gmail.com>
2021-08-07 20:19:16 +0300

    All done finally + часть комментов

---
+++ 16_3_Классы/Temp.py
@@ -0,0 +1,16 @@
+class User:
+    number_of_fingers = 5
+    number_of_eyes =2
+
+
+lancelot = User()
+print(lancelot.number_of_eyes)
+
+peter = User()
+peter.name = 'Peter Robinson'
+
+julia = User
+julia.name = 'Julia Donaldson'
+
+peter.email = 'peterrobimson@mail.ru'
+print(peter.email)
---
+++ 16_4_init/Temp.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
+peter = User(name='peter Robin', email='peterrobinson@mail.com')
+julia = User(name='Juliana Vadfacdf', email='jdvh@kd.xov')
+
+print(peter.name)
+print(julia.email)
